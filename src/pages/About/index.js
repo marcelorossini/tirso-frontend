9f7e3a925dfee151e87ec73cbfdf909593a72858
Marcelo Rossini <marcelo@Marcelos-iMac.local>
2020-04-23 00:11:04 -0300

    upload

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -12,13 +12,13 @@ export default () => {
                     <br/>
                     <strong>A Empresa</strong>
                     <p>
-                        Temos o objetivo de proporcionar aos alunos um treinamento virtual em um ambiente real de oficina, para que possa executar os serviços de forma pratica, fácil e segura tanto ao técnico, quanto ao cliente.<br/>
+                        Temos o objetivo de proporcionar aos alunos um treinamento virtual em um ambiente real de oficina, para que possa executar os serviços de forma prática, fácil e segura tanto ao técnico, quanto ao cliente.<br/>
                         Com o aquecimento da economia, há mais vendas de motos novas, com a "crise" a maior vendas de motos usadas, em todas as opções é necessário uma demanda de profissionais nessa área de mecânica de motos.
                     </p>
                     <br/>
                     <strong>O Profissional</strong>
-                    <p>A mais de 18 anos no ramo de motocicleta, a mais de 12 anos com empresa própria de peças e oficina para motos de todas as marcas, mais de 6 anos em concessionaria e a mais de 5 anos trabalhando com escola de mecânica de motos, formando profissionais capacitados para o trabalho.
-Nossa maior satisfação é a transferência de conhecimento que acumulamos ao longo desses anos de trabalho e o melhor alem do conhecimento técnico a prática, que podemos estar passando para os nossos alunos.</p>
+                    <p>A mais de 18 anos no ramo de motocicleta, a mais de 12 anos com empresa própria de peças e oficina para motos de todas as marcas, mais de 6 anos em concessionária e a mais de 5 anos trabalhando com escola de mecânica de motos, formando profissionais capacitados para o trabalho.
+Nossa maior satisfação é a transferência de conhecimento que acumulamos ao longo desses anos de trabalho e o melhor além do conhecimento técnico a prática, que podemos estar passando para os nossos alunos.</p>
                 </div>             
                 <div className="profile">
                     <img src={tirso} alt="" />
